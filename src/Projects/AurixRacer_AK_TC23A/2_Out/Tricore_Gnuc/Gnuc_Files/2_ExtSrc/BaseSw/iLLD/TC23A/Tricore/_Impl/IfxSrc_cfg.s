4a5a50cd85cd32b1de003b3b485eb7755ebff9c6
Wootaik Lee <wootaik@gmail.com>
2019-03-01 16:22:25 +0900

    remove generated files

--- src/Projects/AurixRacer_AK_TC23A/2_Out/Tricore_Gnuc/Gnuc_Files/2_ExtSrc/BaseSw/iLLD/TC23A/Tricore/_Impl/IfxSrc_cfg.s
+++
@@ -1,8 +0,0 @@
-	.file	"IfxSrc_cfg.c"
-.section .text,"ax",@progbits
-.Ltext0:
-.Letext0:
-.section .debug_line,"",@progbits
-.Ldebug_line0:
-.section .debug_str,"",@progbits
-	.ident	"GCC: (HighTec Release HDP-v4.9.1.0-infineon-2.0-df254e8) 4.9.4 build on 2018-04-18"
